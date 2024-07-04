2e49d2b967f95cf53eb3b1983f41f5b8d362c191
Кирилл Попов <diplomatgmg@gmail.com>
2024-07-04 16:55:39 +0300

    Обновлен ответ при авторизации

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -1,10 +1,10 @@
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenRefreshView
 from django.urls import path
 
-from .views import UserCreateAPIView
+from .views import UserCreateAPIView, CustomTokenObtainPairView
 
 urlpatterns = [
     path("register/", UserCreateAPIView.as_view()),
-    path("token/", TokenObtainPairView.as_view()),
+    path("token/", CustomTokenObtainPairView.as_view()),
     path("token/refresh/", TokenRefreshView.as_view()),
 ]
--- backend/apps/users/views.py
+++ backend/apps/users/views.py
@@ -1,6 +1,17 @@
 from rest_framework.generics import CreateAPIView
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.request import Request
+from rest_framework.response import Response
 
 from .serializers import UserSerializer
+
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    def post(self, request: Request, *args, **kwargs) -> Response:
+        response = super().post(request, *args, **kwargs)
+        email = request.data.get("email")
+
+        return Response({"email": email, "token": response.data})
 
 
 class UserCreateAPIView(CreateAPIView):
