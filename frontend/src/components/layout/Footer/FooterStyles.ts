6b267d37e57f1c57368a30391b304e4fd762defb
Кирилл Попов <diplomatgmg@gmail.com>
2024-07-08 14:20:07 +0300

    Обновлены стили footer

--- frontend/src/components/layout/Footer/FooterStyles.ts
+++ frontend/src/components/layout/Footer/FooterStyles.ts
@@ -4,8 +4,5 @@ export const FooterStyle = styled.footer`
   background-color: #333;
   padding: 1rem;
   text-align: center;
-  position: fixed;
-  bottom: 0;
-  left: 0;
-  right: 0;
+  margin-top: auto;
 `
