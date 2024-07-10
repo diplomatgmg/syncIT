4f1c416146feeb3ac7301b7ce296f0bd06276a36
Кирилл Попов <diplomatgmg@gmail.com>
2024-07-10 21:41:17 +0300

    Обновлены миграции

--- backend/apps/company/migrations/0001_initial.py
+++ backend/apps/company/migrations/0001_initial.py
@@ -12,10 +12,10 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='Grade',
+            name='Company',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=16, unique=True)),
+                ('name', models.CharField(max_length=255)),
             ],
         ),
     ]
