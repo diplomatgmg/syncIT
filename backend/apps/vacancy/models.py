e72d2dda2545abaa7135668bb8b84f17dd5b0709
Кирилл Попов <diplomatgmg@gmail.com>
2024-07-10 21:48:58 +0300

    Обновлено поле work_format

--- backend/apps/vacancy/models.py
+++ backend/apps/vacancy/models.py
@@ -3,19 +3,14 @@ from django.db import models
 from ..company.models import Company
 from ..grade.models import Grade
 from ..hard_skill.models import HardSkill
+from ..work_format.models import WorkFormat
 
 
 class Vacancy(models.Model):
-    FORMAT_CHOICES = (
-        ("r", "Удаленка"),
-        ("o", "Офис"),
-        ("h", "Гибрид"),
-    )
-
     name = models.CharField(max_length=255)
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
     grades = models.ManyToManyField(Grade, related_name="vacancies")
-    format = models.CharField(max_length=16, choices=FORMAT_CHOICES)
+    work_formats = models.ManyToManyField(WorkFormat, related_name="vacancies")
     hard_skills = models.ManyToManyField(HardSkill, related_name="vacancies")
 
     def __str__(self):
