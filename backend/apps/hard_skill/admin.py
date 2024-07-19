b82bc489abadc3bf1f9081c0fba234ddb6a3c013
Кирилл Попов <diplomatgmg@gmail.com>
2024-07-19 12:09:43 +0300

    Улучшена админа

--- backend/apps/hard_skill/admin.py
+++ backend/apps/hard_skill/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 
 from .models import HardSkill
+from ..vacancy.models import Vacancy
 
 
 @admin.register(HardSkill)
 class HardSkillAdmin(admin.ModelAdmin):
-    pass
+    def vacancies_with_skill(self, obj):
+        vacancies = Vacancy.objects.filter(hard_skills=obj)
+        return len(vacancies)
+
+    vacancies_with_skill.short_description = "Vacancies with this skill"
+
+    list_display = ["name", "vacancies_with_skill"]
--- backend/apps/vacancy/admin.py
+++ backend/apps/vacancy/admin.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
 
 from .models import Vacancy
+from django.contrib.admin.filters import SimpleListFilter
+from ..hard_skill.models import HardSkill
+
+
+class HardSkillFilter(SimpleListFilter):
+    title = "Hard Skills"
+    parameter_name = "hard_skills"
+
+    def lookups(self, request, model_admin):
+        return [(skill.id, skill.name) for skill in HardSkill.objects.all()]
+
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(hard_skills__id=self.value())
+        return queryset
 
 
 @admin.register(Vacancy)
 class VacancyAdmin(admin.ModelAdmin):
-    pass
+    list_filter = ("profession__name", HardSkillFilter)
