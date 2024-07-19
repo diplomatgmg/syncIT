c08ed361aab86efe5060a38400aa1c283c41aaa6
Кирилл Попов <diplomatgmg@gmail.com>
2024-07-19 12:10:00 +0300

    Обновлены модели

--- backend/apps/hard_skill/models.py
+++ backend/apps/hard_skill/models.py
@@ -6,3 +6,6 @@ class HardSkill(models.Model):
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        ordering = ("name",)
--- backend/apps/vacancy/models.py
+++ backend/apps/vacancy/models.py
@@ -3,15 +3,27 @@ from django.db import models
 from ..company.models import Company
 from ..grade.models import Grade
 from ..hard_skill.models import HardSkill
+from ..profession.models import Profession
 from ..work_format.models import WorkFormat
 
 
 class Vacancy(models.Model):
+    unique_hash = models.CharField(
+        max_length=64, unique=True, blank=True, null=True, editable=False
+    )
     name = models.CharField(max_length=255)
+    description = models.TextField()
+    salary_from = models.IntegerField(blank=True, null=True)
+    salary_to = models.IntegerField(blank=True, null=True)
+    experience = models.CharField(max_length=255)
+    url = models.URLField()
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
-    grades = models.ManyToManyField(Grade, related_name="vacancies")
+    grade = models.ForeignKey(Grade, on_delete=models.CASCADE)
     work_formats = models.ManyToManyField(WorkFormat, related_name="vacancies")
     hard_skills = models.ManyToManyField(HardSkill, related_name="vacancies")
+    profession = models.ForeignKey(Profession, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    published_at = models.DateTimeField()
 
     def __str__(self):
         return self.name
