a360408a8f620aa0d6873849cb11a29084c87ae4
Кирилл Попов <diplomatgmg@gmail.com>
2024-07-28 00:12:25 +0300

    Добавлены профессии

--- backend/utils/parsers/normalize_profession.py
+++ backend/utils/parsers/normalize_profession.py
@@ -2,6 +2,13 @@ def normalize_profession(profession: str):
     match profession.lower():
         case "devops" | "devops engineer":
             return "DevOps"
+        case "qa automation engineer" | "qa-инженер":
+            return "Тестировщик"
+        case "мобильный разработчик" | "mobile-разработчик":
+            return "Мобильный разработчик"
+        case "project manager" | "it project manager":
+            return "Project Manager"
+        case "data scientist":
+            return "Data Scientist"
         case _:
             return profession
-
