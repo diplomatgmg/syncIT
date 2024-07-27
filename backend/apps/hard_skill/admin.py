757093b06d7a3ad6ebb54b75166f6a2253abe246
Кирилл Попов <diplomatgmg@gmail.com>
2024-07-28 01:18:09 +0300

    Рефакторинг

--- backend/apps/hard_skill/admin.py
+++ backend/apps/hard_skill/admin.py
@@ -1,18 +1,16 @@
 from django.contrib import admin
 
 from .models import HardSkill, UnknownHardSkill
-from ..vacancy.models import Vacancy
 
 
 @admin.register(HardSkill)
 class HardSkillAdmin(admin.ModelAdmin):
-    def vacancies_with_skill(self, obj):
-        vacancies = Vacancy.objects.filter(hard_skills=obj)
-        return len(vacancies)
+    list_display = ("name", "vacancies_count")
 
-    vacancies_with_skill.short_description = "Vacancies with this skill"
+    def vacancies_count(self, obj):
+        return obj.vacancies.count()
 
-    list_display = ["name", "vacancies_with_skill"]
+    vacancies_count.short_description = "Количество вакансий"
 
 
 @admin.register(UnknownHardSkill)
