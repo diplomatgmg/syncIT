b2144c3d15f33b4d5e7500c44b8c4dd875b125ed
Кирилл Попов <diplomatgmg@gmail.com>
2024-07-28 19:05:18 +0300

    Отображаются только selectable hard_skills

--- backend/apps/user_profile/admin.py
+++ backend/apps/user_profile/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 
 from .models import Profile
+from ..hard_skill.models import HardSkill
 
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
-    pass
+    def formfield_for_manytomany(self, db_field, request, **kwargs):
+        if db_field.name == "hard_skills":
+            kwargs["queryset"] = HardSkill.objects.filter(selectable=True)
+        return super().formfield_for_manytomany(db_field, request, **kwargs)
