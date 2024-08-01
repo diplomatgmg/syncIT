59c6ad954834157d6a3d66a2e8ee969a19d3d4f1
Кирилл Попов <diplomatgmg@gmail.com>
2024-08-01 22:52:10 +0300

    Улучшены сообщения об ошибках

--- backend/apps/user/serializers.py
+++ backend/apps/user/serializers.py
@@ -5,11 +5,33 @@ User = get_user_model()
 
 
 class UserCreateSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(max_length=128, min_length=8, write_only=True)
+    email = serializers.EmailField(
+        error_messages={
+            "required": "Email обязателен",
+            "invalid": "Введите корректный адрес электронной почты",
+        }
+    )
+
+    password = serializers.CharField(
+        max_length=128,
+        min_length=8,
+        write_only=True,
+        error_messages={
+            "required": "Пароль обязателен",
+            "min_length": "Пароль должен содержать минимум 8 символов",
+        },
+    )
 
     class Meta:
         model = User
         fields = ("email", "password")
+
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError(
+                "Пользователь с таким email уже существует"
+            )
+        return value
 
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
--- backend/apps/user/views.py
+++ backend/apps/user/views.py
@@ -1,17 +1,24 @@
+from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.generics import CreateAPIView
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-
+from rest_framework import status
 from .serializers import UserCreateSerializer
 
 
 class CustomTokenObtainPairView(TokenObtainPairView):
-    def post(self, request: Request, *args, **kwargs) -> Response:
-        response = super().post(request, *args, **kwargs)
-        email = request.data.get("email")
+    def post(self, request, *args, **kwargs):
+        try:
+            response = super().post(request, *args, **kwargs)
+            email = request.data.get("email")
 
-        return Response({"email": email, "token": response.data})
+            return Response({"email": email, "token": response.data})
+        except AuthenticationFailed:
+            return Response(
+                {"detail": "Пользователя с такими данными не существует"},
+                status=status.HTTP_401_UNAUTHORIZED,
+            )
 
 
 class CustomTokenRefreshView(TokenRefreshView):
