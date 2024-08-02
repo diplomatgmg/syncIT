4f6c782228c6d55e3daf94b8c3963f5a54cbdd08
Кирилл Попов <diplomatgmg@gmail.com>
2024-08-03 01:49:47 +0300

    Оптимизированы запуск CI

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,26 +1,24 @@
-"""
-URL configuration for core project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+
+
+@api_view(["GET"])
+@permission_classes([AllowAny])
+def health_check(_):
+    """
+    Эндпоинт для проверки доступности сервиса.
+    Нужен для корректного запуска celery
+    """
+    return Response({"status": "ok"})
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("health-check/", health_check),
     path("api/", include("apps.user.urls")),
     path("api/", include("apps.hard_skill.urls")),
     path("api/", include("apps.grade.urls")),
