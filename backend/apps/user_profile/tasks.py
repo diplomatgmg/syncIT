911386bc3a745a9ffe015eeb5f0d3b5a1c5fbd0e
Кирилл Попов <diplomatgmg@gmail.com>
2024-08-04 00:24:07 +0300

    Улучшен поиск релевантных вакансий

--- backend/apps/user_profile/tasks.py
+++ backend/apps/user_profile/tasks.py
@@ -1,14 +1,14 @@
-from datetime import datetime, timedelta
+from datetime import timedelta
 
-from celery.app import shared_task
+from celery import shared_task
 from django.conf import settings
 from django.db.models import Count, Q
+from django.utils import timezone
 
-from .models import Profile
-from ..vacancy.models import Vacancy, UserVacancy
+from apps.user_profile.models import Profile
+from apps.vacancy.models import Vacancy, UserVacancy
 
 
-# TODO Переписать таску
 @shared_task()
 def find_suitable_vacancies():
     """
@@ -16,8 +16,8 @@ def find_suitable_vacancies():
     """
     profiles = Profile.objects.filter(is_completed=True)
     newest_vacancies = Vacancy.objects.filter(
-        published_at__gt=datetime.now() - timedelta(days=1)
-    )  # TODO: Django ругается на разницу пользователя и часового пояса
+        published_at__gt=timezone.now() - timedelta(days=1)
+    )
 
     for profile in profiles:
         profile_hard_skills = profile.hard_skills.all()
@@ -25,22 +25,25 @@ def find_suitable_vacancies():
         profile_professions = profile.professions.all()
         profile_grades = profile.grades.all()
 
-        suitable_vacancies = newest_vacancies.annotate(
-            hard_skill_count=Count(
-                "hard_skills", filter=Q(hard_skills__in=profile_hard_skills)
+        suitable_vacancies = (
+            newest_vacancies.filter(
+                work_formats__in=profile_work_formats,
+                profession__in=profile_professions,
+                grade__in=profile_grades,
             )
-        ).filter(
-            hard_skill_count__gte=5,
-            work_formats__in=profile_work_formats,
-            profession__in=profile_professions,
-            grade__in=profile_grades,
+            .annotate(
+                hard_skill_count=Count(
+                    "hard_skills", filter=Q(hard_skills__in=profile_hard_skills)
+                ),
+                total_hard_skills=Count("hard_skills"),
+            )
+            .filter(hard_skill_count__gte=5)
         )
 
         for suitable_vacancy in suitable_vacancies:
             matching_skills_count = suitable_vacancy.hard_skill_count
-            total_skills = suitable_vacancy.hard_skills.count()
-            # FIXME Вакансии могут быть >100 релевантности
-            suitability = min(round((matching_skills_count / total_skills) * 100), 100)
+            total_skills = suitable_vacancy.total_hard_skills
+            suitability = round((matching_skills_count / total_skills) * 100)
 
             if suitability < settings.MINIMUM_VACANCY_SUITABILITY:
                 continue
