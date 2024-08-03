2750b7b7d037f60c4c9208b9d65f0840f74f4c58
Кирилл Попов <diplomatgmg@gmail.com>
2024-08-03 12:29:08 +0300

    Оптимизирован CI

--- backend/core/settings/production.py
+++ backend/core/settings/production.py
@@ -3,3 +3,11 @@ from .base import *
 DEBUG = False
 
 ALLOWED_HOSTS = ["localhost"]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:3000",
+]
