c6e27bb0cc1406b30f79e6aa6e9384d789266a5f
Кирилл Попов <diplomatgmg@gmail.com>
2024-08-04 21:56:23 +0300

    Улучшен нормализатор

--- backend/utils/parsers/normalize_grade.py
+++ backend/utils/parsers/normalize_grade.py
@@ -1,2 +1,8 @@
 def normalize_grade(grade: str):
-    return grade.replace("+", "").replace("-", "/").replace(", ", "/").replace(",", "/")
+    return (
+        grade.replace("+", "")  # Junior+ > Junior
+        .replace("-", "/")  # Junior-Middle > Junior/Middle
+        .replace(", ", "/")  # Junior, Middle > Junior/Middle
+        .replace(",", "/")  # Junior,Middle > Junior/Middle
+        .split()[0]  # Junior Backend Developer > Junior
+    )
