358d5dabff9eab02286af157687ca040908870b2
Кирилл Попов <diplomatgmg@gmail.com>
2024-08-04 22:02:37 +0300

    Улучшен startup.sh

--- backend/startup/development.sh
+++ backend/startup/development.sh
@@ -9,6 +9,10 @@ echo "Migrate database..."
 python manage.py migrate
 echo "Database migrated"
 
+echo "Create superuser..."
+python manage.py createsuperuser --noinput
+echo "Superuser created"
+
 echo "Loading fixtures..."
 python manage.py create_hard_skills
 python manage.py loaddata $FIXTURES_DIR/grades.json
@@ -16,11 +20,8 @@ python manage.py loaddata $FIXTURES_DIR/companies.json
 python manage.py loaddata $FIXTURES_DIR/work_formats.json
 python manage.py loaddata $FIXTURES_DIR/professions.json
 python manage.py loaddata $FIXTURES_DIR/vacancies.json
+python manage.py loaddata $FIXTURES_DIR/user_profiles.json
 echo "Fixtures loaded"
-
-echo "Create superuser..."
-python manage.py createsuperuser --noinput
-echo "Superuser created"
 
 echo "Starting server..."
 python manage.py runserver_plus 0.0.0.0:8000
