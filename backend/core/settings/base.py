91a0f4853688bf1f658d69def32697506da1343b
Кирилл Попов <diplomatgmg@gmail.com>
2024-08-06 23:26:01 +0300

    Session > Basic Auth

--- backend/core/settings/base.py
+++ backend/core/settings/base.py
@@ -65,8 +65,8 @@ MIDDLEWARE = [
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
     "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework.authentication.BasicAuthentication",
         "rest_framework_simplejwt.authentication.JWTAuthentication",
-        "rest_framework.authentication.SessionAuthentication",
     ),
     "DEFAULT_RENDERER_CLASSES": [
         "utils.renderers.CamelCaseJSONRenderer",
