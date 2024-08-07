d79a12ff1dbc8e48d5a0c935c9b7f141add19918
Кирилл Попов <diplomatgmg@gmail.com>
2024-08-07 11:47:27 +0300

    Фикс авторизации для django rest

--- backend/core/settings/base.py
+++ backend/core/settings/base.py
@@ -65,7 +65,6 @@ MIDDLEWARE = [
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
     "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework.authentication.BasicAuthentication",
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
     "DEFAULT_RENDERER_CLASSES": [
