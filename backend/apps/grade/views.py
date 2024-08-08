31d6a28ff3b007fb4694762c9c41807df0152442
Кирилл Попов <diplomatgmg@gmail.com>
2024-08-09 00:52:55 +0300

    Добавлено кеширование

--- backend/apps/grade/views.py
+++ backend/apps/grade/views.py
@@ -1,9 +1,12 @@
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 from rest_framework import generics
 
 from .models import Grade
 from .serializers import GradeSerializer
 
 
+@method_decorator(cache_page(600), name="dispatch")  # 10 Минут
 class GradeListAPIView(generics.ListAPIView):
     queryset = Grade.objects.all()
     serializer_class = GradeSerializer
--- backend/apps/profession/views.py
+++ backend/apps/profession/views.py
@@ -1,9 +1,12 @@
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 from rest_framework import generics
 
 from .models import Profession
 from .serializers import ProfessionSerializer
 
 
+@method_decorator(cache_page(600), name="dispatch")  # 10 Минут
 class ProfessionListAPIView(generics.ListAPIView):
     queryset = Profession.objects.all()
     serializer_class = ProfessionSerializer
