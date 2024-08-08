ad501190d3638dd2b4d8190c875ebb3ae59a6e4b
Кирилл Попов <diplomatgmg@gmail.com>
2024-08-09 00:32:21 +0300

    Обновлены настройки под https

--- backend/core/settings/production.py
+++ backend/core/settings/production.py
@@ -7,13 +7,23 @@ ALLOWED_HOSTS = [
     "syncit.space",
 ]
 
-
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost",
-    "http://syncit.space/",
+    "https://localhost",
+    "https://syncit.space",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    "http://localhost",
-    "http://syncit.space/",
+    "https://localhost",
+    "https://syncit.space",
 ]
+
+SECURE_SSL_REDIRECT = True
+SECURE_HSTS_SECONDS = 31536000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
