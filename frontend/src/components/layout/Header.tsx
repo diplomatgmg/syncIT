252fed222e2dc2b29acd2ca59431b852b4a5d58a
Kirill Popov <diplomatgmg@gmail.com>
2024-08-13 22:15:03 +0300

    Улучшен адаптив для header

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -1,4 +1,4 @@
-import styled from "styled-components"
+import styled, { ThemeProvider } from "styled-components"
 import { Box, Button, Flex, Group, Text, useMantineTheme } from "@mantine/core"
 import SyncItImg from "@/assets/img/syncIt.png"
 import { Link, useNavigate } from "react-router-dom"
@@ -6,7 +6,7 @@ import routes from "@/routes/routes.tsx"
 import useAuth from "@/store/hooks/useAuth.ts"
 import useLogout from "@/store/hooks/useLogout.ts"
 import { useMediaQuery } from "@mantine/hooks"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 const Header = () => {
   const { isAuthenticated, email } = useAuth()
@@ -15,6 +15,19 @@ const Header = () => {
   const theme = useMantineTheme()
   const matches = useMediaQuery("(max-width: 1024px)")
   const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  useEffect(() => {
+    // Отключение скролла при открытом меню
+    if (isMenuOpen) {
+      document.body.style.position = "fixed"
+    } else {
+      document.body.style.overflowX = ""
+    }
+
+    return () => {
+      document.body.style.overflowX = ""
+    }
+  }, [isMenuOpen])
 
   const handleLogout = () => {
     logoutHandler()
@@ -25,11 +38,11 @@ const Header = () => {
     setIsMenuOpen(!isMenuOpen)
   }
 
-  const renderAuthButtons = (mobile: boolean = false) => {
+  const renderAuthButtons = () => {
     if (isAuthenticated) {
       return (
         <Group>
-          {!mobile && <Text>{email}</Text>}
+          <Text>{email}</Text>
           <Button color={"red"} onClick={handleLogout}>
             Выйти
           </Button>
@@ -37,50 +50,45 @@ const Header = () => {
       )
     }
     return (
-      <Group>
-        <Button
-          component={Link}
-          to={routes.login.path}
-          size={mobile ? "xl" : "md"}>
+      <Flex wrap={"wrap"} justify={"center"} gap={"md"}>
+        <Button component={Link} to={routes.login.path}>
           Войти
         </Button>
-        <Button
-          variant="default"
-          component={Link}
-          to={routes.register.path}
-          size={mobile ? "xl" : "md"}>
+        <Button variant="default" component={Link} to={routes.register.path}>
           Регистрация
         </Button>
-      </Group>
+      </Flex>
     )
   }
 
   const renderMobileMenu = () => {
     return (
       <>
-        <Group>
-          <Text>{email}</Text>
+        <Flex align={"center"} gap={"sm"} wrap={"nowrap"}>
           <Button onClick={toggleMenu} w={"50px"}>
             {isMenuOpen ? "✕" : "☰"}
           </Button>
-        </Group>
+        </Flex>
         {isMenuOpen && (
           <MobileMenu>
             <Flex
+              mt={"md"}
               direction={"column"}
               align={"center"}
               gap={theme.fontSizes.xs}>
-              <LinkStyle to={routes.home.path} style={{ fontSize: "2rem" }}>
+              <LinkStyle to={routes.home.path} style={{ fontSize: "1.75rem" }}>
                 Вакансии
               </LinkStyle>
-              <LinkStyle to={routes.profile.path} style={{ fontSize: "2rem" }}>
+              <LinkStyle
+                to={routes.profile.path}
+                style={{ fontSize: "1.75rem" }}>
                 Профиль
               </LinkStyle>
-              <LinkStyle to={routes.faq.path} style={{ fontSize: "2rem" }}>
+              <LinkStyle to={routes.faq.path} style={{ fontSize: "1.75rem" }}>
                 FAQ
               </LinkStyle>
             </Flex>
-            <Flex justify={"center"}>{renderAuthButtons(true)}</Flex>
+            <Flex justify={"center"}>{renderAuthButtons()}</Flex>
           </MobileMenu>
         )}
       </>
@@ -101,17 +109,19 @@ const Header = () => {
   }
 
   return (
-    <Box pb="mb">
-      <HeaderStyle>
-        <Group justify="space-between" h="100%">
-          <Link to={routes.home.path}>
-            <LogoStyle src={SyncItImg} alt="syncIT" />
-          </Link>
-          {matches && renderMobileMenu()}
-          {!matches && renderDesktopMenu()}
-        </Group>
-      </HeaderStyle>
-    </Box>
+    <ThemeProvider theme={{ isMenuOpen }}>
+      <Box pb="mb">
+        <HeaderStyle>
+          <Group justify="space-between" h="100%">
+            <Link to={routes.home.path}>
+              <LogoStyle src={SyncItImg} alt="syncIT" />
+            </Link>
+            {matches && renderMobileMenu()}
+            {!matches && renderDesktopMenu()}
+          </Group>
+        </HeaderStyle>
+      </Box>
+    </ThemeProvider>
   )
 }
 
