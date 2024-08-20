1877e03489dcc313bbaea466e44c2b13f7883fa2
Kirill Popov <diplomatgmg@gmail.com>
2024-08-20 23:14:55 +0300

    Убрано кеширование

--- backend/apps/hard_skill/serializers.py
+++ backend/apps/hard_skill/serializers.py
@@ -19,13 +19,6 @@ class HardSkillRecursiveSerializer(serializers.ModelSerializer):
 
     def get_children(self, obj: HardSkill):
         # Мб можно обойтись без кеширования
-        cache_key = f"hard_skill_{obj.id}_children"
-        cached_children = cache.get(cache_key)
-
-        if cached_children is not None:
-            return cached_children
-
         children = obj.children.all()
         serialized_children = HardSkillRecursiveSerializer(children, many=True).data
-        cache.set(cache_key, serialized_children, timeout=600)  # 10 Минут
         return serialized_children
