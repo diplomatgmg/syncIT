b72c578b1b216f6ed8dffc958e1d3e83f71091f6
Kirill Popov <diplomatgmg@gmail.com>
2024-08-30 21:57:42 +0300

    Добавлены тесты

---
+++ backend/helpers/utils/tests/test_normalize_profession.py
@@ -0,0 +1,33 @@
+from django.test import TestCase
+
+from helpers.utils.normalize_profession import normalize_profession
+
+
+class TestNormalizeProfession(TestCase):
+    def test_exact_match(self):
+        self.assertEqual(normalize_profession("devops"), "DevOps")
+        self.assertEqual(
+            normalize_profession("бэкенд-разработчик"), "Backend-разработчик"
+        )
+        self.assertEqual(normalize_profession("qa automation engineer"), "Тестировщик")
+
+    def test_fuzzy_match(self):
+        self.assertEqual(normalize_profession("DevOps engineer"), "DevOps")
+        self.assertEqual(
+            normalize_profession("Senior бэкенд-разработчик"), "Backend-разработчик"
+        )
+        self.assertEqual(normalize_profession("QA Automation Engineer"), "Тестировщик")
+
+    def test_no_match(self):
+        self.assertEqual(
+            normalize_profession("Unknown profession"), "Unknown profession"
+        )
+
+    def test_empty_string(self):
+        self.assertEqual(normalize_profession(""), "")
+
+    def test_case_insensitivity(self):
+        self.assertEqual(normalize_profession("DeVoPs"), "DevOps")
+        self.assertEqual(
+            normalize_profession("БЭКЕНД-РАЗРАБОТЧИК"), "Backend-разработчик"
+        )
