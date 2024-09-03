fbbd5c76a999725f2ef23e3ee34b72506c92a6d6
Кирилл <118633470+diplomatgmg@users.noreply.github.com>
2024-09-03 22:18:08 +0300

    Update profile_attributes_base.py

--- backend/apps/user_profile/views/profile_attributes_base.py
+++ backend/apps/user_profile/views/profile_attributes_base.py
@@ -11,10 +11,10 @@ class ProfileAttributesBaseAPIView(
         super().__init__(*args, **kwargs)
 
         if self.attribute_model is None:
-            raise TypeError("attribute_model must be defined.")
+            raise NotImplementedError("attribute_model must be defined.")
 
         if self.attribute_serializer is None:
-            raise TypeError("attribute_serializer must be defined.")
+            raise NotImplementedError("attribute_serializer must be defined.")
 
         if self.attribute_field is None:
-            raise TypeError("attribute_field must be defined.")
+            raise NotImplementedError("attribute_field must be defined.")
