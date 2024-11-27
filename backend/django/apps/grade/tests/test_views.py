b19e6df16843dec5ac7136efab9e1a44bd202905
Kirill Popov <k.popov@abakam.ru>
2024-11-27 21:31:52 +0300

    fix: tests

--- backend/django/apps/grade/tests/test_views.py
+++ backend/django/apps/grade/tests/test_views.py
@@ -2,21 +2,15 @@ from django.contrib.auth import get_user_model
 from django.urls import reverse
 from rest_framework import status
 
-from apps.grade.models import Grade
-from apps.grade.serializers import GradeSerializer
 from apps.grade.views import GradeProxyAPIView
 from helpers.for_tests import BaseViewTestCase
-
 
 User = get_user_model()
 
 
 class GradeProxyAPIViewTestCase(BaseViewTestCase):
     def test_list_grades(self):
-        """Проверяем, что представление возвращает список всех оценок"""
-        Grade.objects.create(name="Junior")
-        Grade.objects.create(name="Middle")
-
+        """Проверяем, что представление корректно работает с go"""
         url = reverse("grade-list")
         self.assert_unauthorized(url)
         request = self.authenticate_request(url)
@@ -24,7 +18,3 @@ class GradeProxyAPIViewTestCase(BaseViewTestCase):
         response = view(request)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 2)
-
-        grades = Grade.objects.all()
-        self.assertCountEqual(response.data, GradeSerializer(grades, many=True).data)
--- backend/django/apps/work_format/tests/test_views.py
+++ backend/django/apps/work_format/tests/test_views.py
@@ -2,8 +2,6 @@ from django.contrib.auth import get_user_model
 from django.urls import reverse
 from rest_framework import status
 
-from apps.work_format.models import WorkFormat
-from apps.work_format.serializers import WorkFormatSerializer
 from apps.work_format.views import WorkFormatProxyAPIView
 from helpers.for_tests import BaseViewTestCase
 
@@ -12,10 +10,7 @@ User = get_user_model()
 
 class WorkFormatProxyAPIViewTestCase(BaseViewTestCase):
     def test_list_work_formats(self):
-        """Проверяем, что представление возвращает список всех форматов работы"""
-        WorkFormat.objects.create(name="Офис")
-        WorkFormat.objects.create(name="Удаленка")
-
+        """Проверяем, что представление корректно работает с go"""
         url = reverse("work-format-list")
         self.assert_unauthorized(url)
         request = self.authenticate_request(url)
@@ -23,9 +18,3 @@ class WorkFormatProxyAPIViewTestCase(BaseViewTestCase):
         response = view(request)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 2)
-
-        work_formats = WorkFormat.objects.all()
-        self.assertCountEqual(
-            response.data, WorkFormatSerializer(work_formats, many=True).data
-        )
