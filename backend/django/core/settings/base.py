8b659f5231d8d7fd618f7e8bf9fae8dee65a819f
Kirill Popov <diplomatgmg@gmail.com>
2024-12-10 16:54:02 +0300

    feat: user reg.ru smtp mail

--- backend/django/core/settings/base.py
+++ backend/django/core/settings/base.py
@@ -85,7 +85,7 @@ SIMPLE_JWT = {
 
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.yandex.ru"
+EMAIL_HOST = "sm15.hosting.reg.ru"
 EMAIL_PORT = 465
 EMAIL_USE_SSL = True
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
