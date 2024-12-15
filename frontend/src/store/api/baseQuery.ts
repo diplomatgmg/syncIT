dc74b8f010de103eb2624d1cc3523d22112f7352
Kirill Popov <diplomatgmg@gmail.com>
2024-12-15 20:26:57 +0300

    feat: added mutex. token refresh logic updated

--- frontend/src/store/api/baseQuery.ts
+++ frontend/src/store/api/baseQuery.ts
@@ -2,8 +2,11 @@ import { BaseQueryFn, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 import { RootState } from "@/store/store.ts"
 import { logout, setTokens } from "@/store/slice/authSlice.ts"
 import { LoginResponse } from "@/types/authTypes.ts"
+import { Mutex } from "async-mutex"
 
 const API_URL = import.meta.env.PROD ? "/api" : "http://localhost:8000/api"
+
+const mutex = new Mutex()
 
 const baseQuery = fetchBaseQuery({
   baseUrl: API_URL,
@@ -19,14 +22,11 @@ const baseQuery = fetchBaseQuery({
 })
 
 const baseQueryWithReauth: BaseQueryFn = async (args, api, extraOptions) => {
+  await mutex.waitForUnlock()
+
   let result = await baseQuery(args, api, extraOptions)
 
-  if (result.error && result.error.status !== 401) {
-    console.error(result.error)
-    return result
-  }
-
-  if (!(result.error && result.error.status === 401)) {
+  if (!result.error || result.error.status !== 401) {
     return result
   }
 
@@ -37,21 +37,31 @@ const baseQueryWithReauth: BaseQueryFn = async (args, api, extraOptions) => {
     return result
   }
 
-  const refreshResult = await baseQuery(
-    {
-      url: "token/refresh/",
-      method: "POST",
-      body: { refresh: refreshToken },
-    },
-    api,
-    extraOptions
-  )
+  if (!mutex.isLocked()) {
+    const release = await mutex.acquire()
+    try {
+      const refreshResult = await baseQuery(
+        {
+          url: "token/refresh/",
+          method: "POST",
+          body: { refresh: refreshToken },
+        },
+        api,
+        extraOptions
+      )
 
-  if (refreshResult.data) {
-    api.dispatch(setTokens(refreshResult.data as LoginResponse))
-    result = await baseQuery(args, api, extraOptions)
+      if (refreshResult.data) {
+        api.dispatch(setTokens(refreshResult.data as LoginResponse))
+        result = await baseQuery(args, api, extraOptions)
+      } else {
+        api.dispatch(logout())
+      }
+    } finally {
+      release()
+    }
   } else {
-    api.dispatch(logout())
+    await mutex.waitForUnlock()
+    result = await baseQuery(args, api, extraOptions)
   }
 
   return result
