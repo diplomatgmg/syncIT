fb344bb2a676d2d2705ee658fac06ff49204e3ad
Kirill Popov <diplomatgmg@gmail.com>
2024-12-19 03:04:57 +0300

    feat: used gpt api

--- backend/django/parsers/open_ai/chat_gpt.py
+++ backend/django/parsers/open_ai/chat_gpt.py
@@ -1,9 +1,8 @@
 import logging
+import os
 import re
-import time
 
-import g4f
-import g4f.Provider
+from openai import OpenAI
 
 logger = logging.getLogger("django")
 
@@ -13,32 +12,13 @@ def clear_text(text: str) -> str:
 
 
 def get_chat_gpt_completion(prompt: str) -> str | None:
-    def get_completion(_attempt: int) -> str | None:
-        try:
-            client = g4f.client.Client()
-            response = client.chat.completions.create(
-                model=g4f.models.gpt_35_turbo,
-                provider=g4f.Provider.ChatGptEs,
-                messages=[{"role": "user", "content": prompt}],
-            )
-            return clear_text(response.choices[0].message.content)  # noqa
-        except Exception as e:
-            logger.warning(
-                f"Попытка {_attempt}: Не удалось получить ответ от провайдера: {e}",
-                exc_info=True,
-            )
-            return None
-
-    attempt = 1
-    while attempt <= 5:
-        completion = get_completion(attempt)
-        if completion is not None:
-            return completion
-        time.sleep(15)
-        attempt += 1
-
-    logger.error(
-        "Не удалось получить ответ после 5 попыток.",
-        exc_info=True,
+    client = OpenAI(
+        api_key=os.getenv("GPT_API_KEY"),
+        base_url="https://api.aitunnel.ru/v1/",
     )
-    return None
+    response = client.chat.completions.create(
+        messages=[{"role": "user", "content": prompt}],
+        model="gpt-4o-mini",
+    )
+
+    return clear_text(response.choices[0].message.content)  # noqa
