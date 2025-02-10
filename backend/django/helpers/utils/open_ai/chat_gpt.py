b8e546c487aaf6ea4a92ae92c1f2b0388969130f
Kirill Popov <diplomatgmg@gmail.com>
2025-02-11 01:55:41 +0300

    refactor: changed GPT model
    Took 14 seconds

--- backend/django/helpers/utils/open_ai/chat_gpt.py
+++ backend/django/helpers/utils/open_ai/chat_gpt.py
@@ -19,7 +19,7 @@ def get_chat_gpt_completion(prompt: str) -> str | None:
         try:
             client = Client()
             response = client.chat.completions.create(
-                model=models.blackboxai,
+                model=models.gemini_2_0_flash,
                 provider=Provider.Blackbox,
                 messages=[{"role": "user", "content": prompt}],
             )
