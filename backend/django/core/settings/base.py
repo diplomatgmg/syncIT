843751098e15553cb2ffe1d44d8f499e4b3baf00
Kirill Popov <diplomatgmg@gmail.com>
2025-02-14 19:55:10 +0300

    refactor: changed time token lifetime
    Took 11 minutes

--- backend/django/core/settings/base.py
+++ backend/django/core/settings/base.py
@@ -79,8 +79,8 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=4),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
 }
