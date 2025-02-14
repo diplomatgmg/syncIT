336944634e217bac1ab620bfc821131658e300f8
Kirill Popov <diplomatgmg@gmail.com>
2025-02-14 16:59:17 +0300

    feat: added order field
    Took 10 minutes

--- backend/django/apps/grade/models.py
+++ backend/django/apps/grade/models.py
@@ -3,6 +3,10 @@ from django.db import models
 
 class Grade(models.Model):
     name = models.CharField(max_length=16, unique=True)
+    order = models.IntegerField(null=True, blank=True)
+
+    class Meta:
+        ordering = ("order",)
 
     def __str__(self):
         return self.name
--- backend/django/apps/profession/models/profession.py
+++ backend/django/apps/profession/models/profession.py
@@ -3,6 +3,10 @@ from django.db import models
 
 class Profession(models.Model):
     name = models.CharField(max_length=100, unique=True)
+    order = models.IntegerField(null=True, blank=True)
+
+    class Meta:
+        ordering = ("order",)
 
     def __str__(self):
         return self.name
