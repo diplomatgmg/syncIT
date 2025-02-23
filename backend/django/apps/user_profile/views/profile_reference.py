eb100e35ec45b7a26532d62a0f7ab4a5d308a99b
Kirill Popov <diplomatgmg@gmail.com>
2025-02-23 16:41:59 +0300

    fix cache
    Took 16 minutes

--- backend/django/apps/user_profile/views/profile_reference.py
+++ backend/django/apps/user_profile/views/profile_reference.py
@@ -1,10 +1,22 @@
-from django.utils.decorators import method_decorator
-from django.views.decorators.cache import cache_page
+from django.core.cache import cache
+from rest_framework.request import Request
+from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from helpers.mixins.views import ProxyAPIMixin
 
 
-@method_decorator(cache_page(24 * 60 * 60), name="dispatch")
 class ProfileReferenceAPIView(ProxyAPIMixin, APIView):
     proxy_path = "/api/profile/reference"
+
+    def get(self, request: Request, *args, **kwargs) -> Response:
+        cache_key = "profile_reference"
+        cached_data = cache.get(cache_key)
+
+        if cached_data is not None:
+            return Response(cached_data)
+
+        response = super().get(request, *args, **kwargs)
+        cache.set(cache_key, response.data, 60 * 15)
+
+        return response
